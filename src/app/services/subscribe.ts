a73bbe97e8109b3d35ada9f54d5aa2f30afa61a2
natdev <dordonne.nathanael@gmail.com>
2025-12-15 00:39:48 -0400

    change api url

--- src/app/services/subscribe.ts
+++ src/app/services/subscribe.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,7 @@ import { Observable } from 'rxjs';
 export class Subscribe {
    private http = inject(HttpClient);
   subscription(username: string, password: string): Observable<any> {
-    const url = 'http://localhost:3000/subscribe';
+    const url = `${environment.apiUrl}/subscribe`;
     return this.http.post(url, { username, password });
   }
 }
